e9bcda716c9c2a41ae06eaa7668ed0e6d4d6bc3d
Ram Jothikumar <ram@signalfuse.com>
2015-09-03 16:39:36 -0700

    Adding puppetmaster, agent install shell scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 mkdir /root/puppet_agent
 cd /root/puppet_agent
-wget https://raw.githubusercontent.com/ramjothikumar/puppet_setup/master/Dockerfile 
-docker build -t puppet_agent -f Dockerfile
-
+wget https://raw.githubusercontent.com/ramjothikumar/puppet_setup/master/Dockerfile
+docker build -t puppet-agent /root/puppet_agent
