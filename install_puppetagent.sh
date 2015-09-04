5ff4e31b8a12630608225f90b9d83ff99f4b850f
Ram Jothikumar <ram@signalfuse.com>
2015-09-03 21:29:09 -0700

    fixes

--- install_puppetagent.sh
+++ install_puppetagent.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
 puppet_master_ip="$1"
+if [ "$puppet_master_ip" == "" ]; then
+    echo "Usage: install_puppetagent.sh <puppet_master_ip>"
+    echo "Puppet Master IP Address not Provided. Exiting"
+    exit 0
+fi
+echo "Puppet Master IP: $puppet_master_ip"
 
 # Determine if the script is being run by root or not
 user=$(whoami)
@@ -12,13 +18,14 @@ fi
 
 # Determine hostOS
 hostOS=$(cat /etc/*-release | grep PRETTY_NAME | grep -o '".*"' | sed 's/"//g' | sed -e 's/([^()]*)//g' | sed -e 's/[[:space:]]*$//')
-if [ ! -f /etc/redhat-release ]; then
+if [ -f /etc/redhat-release ]; then
     hostOS=$(head -c 16 /etc/redhat-release)
 fi
+echo "HostOS Flavor: $hostOS"
 
-if [ "$hostOS" == *"Ubuntu"* ]; then
+# Install Puppet
+if [[ "$hostOS" =~ "Ubuntu" ]]; then
     $sudo apt-get update -y && apt-get install -y puppet
-
 else
     $sudo yum install -y puppet
     $sudo chkconfig puppet on
--- install_puppetmaster.sh
+++ install_puppetmaster.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/bash
 
+puppet_master_ip="$1"
+if [ "$puppet_master_ip" == "" ]; then
+    echo "Usage: install_puppetagent.sh <puppet_master_ip>"
+    echo "Puppet Master IP Address not Provided. Exiting"
+    exit 0
+fi
+echo "Puppet Master IP: $puppet_master_ip"
+
 # Determine if the script is being run by root or not
 user=$(whoami)
-if [ "$user" == "root" ]; then 
+if [ "$user" == "root" ]; then
     sudo=""
 else
     sudo="sudo"
 fi
-$sudo yum install -y puppet-server
-$sudo chkconfig puppetmaster on
+
+# Determine hostOS
+hostOS=$(cat /etc/*-release | grep PRETTY_NAME | grep -o '".*"' | sed 's/"//g' | sed -e 's/([^()]*)//g' | sed -e 's/[[:space:]]*$//')
+if [ -f /etc/redhat-release ]; then
+    hostOS=$(head -c 16 /etc/redhat-release)
+fi
+echo "HostOS Flavor: $hostOS"
+
+# Install puppet master/server
+if [[ "$hostOS" =~ "Ubuntu" ]]; then
+    $sudo apt-get install -y puppetmaster
+else
+    $sudo yum install -y puppet-server
+    $sudo chkconfig puppetmaster on
+fi
 $sudo service puppetmaster start
